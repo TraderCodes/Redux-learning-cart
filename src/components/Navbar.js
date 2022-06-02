9c4411f6bafa5eeeaa1de61f0f5e06417bedc40d
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-02 16:04:05 -0700

    added "useSelector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,18 @@ import { useSelector } from 'react-redux';
 import React from 'react';
 
 const Navbar = () => {
+  const amount = useSelector((store) => store.cart.amount )
   return (
     <nav>
       <div className="nav-center">
         <h3>Redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcon></CartIcon>
+          <div className="amount-container">
+
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
       </div>
     </nav>
   );
