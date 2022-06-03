38435b285b68631cf16a5780a8d363edae42966c
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-03 13:18:13 -0700

    added : icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
-import { CartIcon } from '../icons';
+import { CartIcon, Testing } from '../icons';
 import { useSelector } from 'react-redux';
 
 import React from 'react';
 
 const Navbar = () => {
-  const amount = useSelector((store) => store.cart.amount )
+  const amount = useSelector((store) => store.cart.amount);
   return (
     <nav>
       <div className="nav-center">
         <h3>Redux toolkit</h3>
         <div className="nav-container">
+          <Testing></Testing>
           <CartIcon></CartIcon>
           <div className="amount-container">
-
             <p className="total-amount">{amount}</p>
           </div>
         </div>
